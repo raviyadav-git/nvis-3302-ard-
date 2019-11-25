f742675eb35d7cb0aae7dd59fb3467007c522829
raviyadav-git <56599051+raviyadav-git@users.noreply.github.com>
2019-11-25 21:34:29 +0530

    arduino .ino
    ir sensor normal line follower

---
+++ irsensornormallinefollower.ino
@@ -0,0 +1,45 @@
+
+int t1=A0;
+int t2=A2;
+void setup() 
+{
+pinMode(pin5,OUTPUT);  
+pinMode(pin6,OUTPUT);  
+pinMode(pin7,OUTPUT);
+pinMode(pin8,OUTPUT);
+Serial.begin(9600);
+}
+void normalLineFollow()
+{
+int r1=digitalRead(t1);
+int r2=digitalRead(t2);
+if(r1==LOW&&r2==LOW)
+{
+  digitalWrite(pin5,HIGH);
+  digitalWrite(pin6,LOW);
+  digitalWrite(pin7,HIGH);
+  digitalWrite(pin8,LOW);
+  
+}
+ if(r1==HIGH&&r2==LOW)
+{
+  digitalWrite(pin5,LOW);
+  digitalWrite(pin6,HIGH);
+  digitalWrite(pin7,HIGH);
+  digitalWrite(pin8,LOW);
+  
+} if(r1==LOW&&r2==HIGH)
+{
+  digitalWrite(pin5,HIGH);
+  digitalWrite(pin6,LOW);
+  digitalWrite(pin7,LOW);
+  digitalWrite(pin8,HIGH);
+ }
+ if(r1==HIGH&&r2==HIGH)
+{
+  digitalWrite(pin5,HIGH);
+  digitalWrite(pin6,LOW);
+  digitalWrite(pin7,HIGH);
+  digitalWrite(pin8,LOW);
+  
+}
